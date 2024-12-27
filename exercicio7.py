d6a6e5444b6dfaa7d7b992925093da7563936318
matheus.antonio <matheusk42@hotmail.com>
2024-12-26 23:41:36 -0300

    Salvando o 7 exercicio

---
+++ exercicio7.py
@@ -0,0 +1,11 @@
+usuarios = [
+    {"nome": "Alice", "email": "alice@example.com"},
+    {"nome": "Bob", "email": ""},
+    {"nome": "Carol", "email": "carol@example.com"}
+]
+
+
+
+usuarios_validos = [usuario for usuario in usuarios if usuario['email']]
+
+print(usuarios_validos)
